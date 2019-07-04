a8a0f7636ecf0faff0e738624179e5cf11675adf
Oğuzhan <oguzhan.karakus2012@gmail.com>
2019-07-04 20:07:05 +0300

    Initial Commit

---
+++ LoodosMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoodosMovie
+//
+//  Created by Oğuzhan Karakuş on 4.07.2019.
+//  Copyright © 2019 Oğuzhan Karakuş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
